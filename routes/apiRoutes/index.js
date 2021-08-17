f61719e17769d299ccb402931d08d6d42b41ced4
JHallUofA <83625694+JHallUofA@users.noreply.github.com>
2021-08-17 09:05:31 -0700

    add api intedgration

--- routes/apiRoutes/index.js
+++ routes/apiRoutes/index.js
@@ -1,6 +1,10 @@
 const router = require('express').Router();
-const animalRoutes = require('./animalRoutes');
+const animalRoutes = require('../apiRoutes/animalRoutes');
+const bp = require('body-parser');
 
+router.use(bp.json());
+router.use(bp.urlencoded({ extended:true }));
 router.use(animalRoutes);
+router.use(require('./zookeeperRoutes'));
 
 module.exports = router;
